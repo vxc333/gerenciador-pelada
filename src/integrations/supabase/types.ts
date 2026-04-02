0b1c97fd44807e40da7993d65c7403938a719147
Vitor Xavier <vitor.xavier.dev@gmail.com>
2026-04-02 12:20:23 -0300

    feat: adicionar campo is_waiting ao tipo Database

--- src/integrations/supabase/types.ts
+++ src/integrations/supabase/types.ts
@@ -20,6 +20,7 @@ export type Database = {
           created_at: string
           guest_name: string
           id: string
+          is_waiting: boolean
           pelada_id: string
           pelada_member_id: string
         }
@@ -28,6 +29,7 @@ export type Database = {
           created_at?: string
           guest_name: string
           id?: string
+          is_waiting?: boolean
           pelada_id: string
           pelada_member_id: string
         }
@@ -36,6 +38,7 @@ export type Database = {
           created_at?: string
           guest_name?: string
           id?: string
+          is_waiting?: boolean
           pelada_id?: string
           pelada_member_id?: string
         }
